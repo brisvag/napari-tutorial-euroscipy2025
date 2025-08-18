da429eb6ab56ab455dbbf4a5e13c6e74843fb29b
Grzegorz Bokota <bokota+github@gmail.com>
2025-08-18 10:58:02 +0200

    move scale bar and reset scale to proper initial view

--- presentation.py
+++ presentation.py
@@ -10,4 +10,6 @@ viewer.open(img_name, scale=(100, 1, 1), interpolation3d='nearest')
 viewer.dims.point = (0, 0, 0)
 get_unit_registry().define('happiness = 1')
 viewer.scale_bar.unit = 'happiness'
+viewer.scale_bar.position = "bottom_left"
+viewer.reset_view()
 napari.run()
